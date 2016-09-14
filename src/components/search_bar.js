2e0587e8fdefe957efd95f1af8c8048e7a2dedcd
Mitchell Deane <mitchelld20@yahoo.com>
2016-09-14 16:53:22 -0500

    added some css for some initial formatting

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,7 +11,7 @@ class SearchBar extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="search-bar">
 			  value: {this.state.term}
               <input onChange={(event) => this.setState({ term: event.target.value})} />
               
